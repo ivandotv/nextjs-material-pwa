7bfc44946390551285d801aa687cb92f31b54953
ivandotv <390700+ivandotv@users.noreply.github.com>
2020-08-18 22:18:37 +0200

    build: implement custom babel configuration

---
+++ .babelrc.js
@@ -0,0 +1,16 @@
+const pkg = require('./package.json')
+
+const pkgVersion = process.env.PKG_VERSION || pkg.version
+
+module.exports = {
+  presets: [['next/babel']],
+  plugins: [
+    [
+      'transform-define',
+      {
+        __VERSION__: pkgVersion,
+        __DEV__: process.env.NODE_ENV !== 'production'
+      }
+    ]
+  ]
+}
