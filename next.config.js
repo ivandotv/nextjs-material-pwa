0153f9a4e31bbba75fb56cf0c4455b0e5e4ccdab
ivandotv <390700+ivandotv@users.noreply.github.com>
2020-08-20 14:56:31 +0200

    build: add runtime pwa caching to config

--- next.config.js
+++ next.config.js
@@ -2,11 +2,12 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
 const withPWA = require('next-pwa')
-// const runtimeCaching = require('next-pwa/cache')
+const runtimeCaching = require('next-pwa/cache')
+
 const nextConfig = {
   pwa: {
     dest: 'public',
-    // runtimeCaching
+    runtimeCaching,
     disable: process.env.NODE_ENV === 'development'
   }
 }
