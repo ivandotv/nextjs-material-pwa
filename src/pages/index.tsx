ccbaab36f08e88c544a7e51caad0f8cf73120165
ivandotv <390700+ivandotv@users.noreply.github.com>
2021-09-16 11:30:59 +0200

    remove custom font from index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link'
-import Head from 'next/head'
 import useStyles from 'styles/landing-page-style'
 
 export default function Index() {
@@ -7,12 +6,6 @@ export default function Index() {
 
   return (
     <>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&display=swap"
-          rel="stylesheet"
-        ></link>
-      </Head>
       <div className={classes.pageWrap}>
         <h1>Next.js Material PWA</h1>
         <div className={classes.bannerWrapper}>
