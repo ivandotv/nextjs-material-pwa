77e11bc46296b439db57305169a0fcb9a037c845
ivandotv <390700+ivandotv@users.noreply.github.com>
2021-12-20 14:52:53 +0100

    add nextconfig typescript check

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
+// @ts-check
 const withPlugins = require('next-compose-plugins')
 const withWorkbox = require('./workbox.webpack.config')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
 
+/** @type {import('next/dist/server/config').NextConfig}*/
 const nextConfig = {
+  // reactStricMode: true,
   workbox: {
     disable: process.env.NODE_ENV !== 'production',
     swSrc: 'src/service-worker/sw.ts',
